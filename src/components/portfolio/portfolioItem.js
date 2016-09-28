865a3ef57f8512058ccc2de3ef44c0498a6149bf
Kelly Carmichael <digikidd@gmail.com>
2016-09-27 22:59:32 -0500

    no message

--- src/components/portfolio/portfolioItem.js
+++ src/components/portfolio/portfolioItem.js
@@ -6,7 +6,7 @@ class PortfolioItem extends React.Component{
         var portfolio = this.props.portfolio;
         var DefaultTitle = "kbc-ninja project";
         var DefaultDescription = "Hong Kong Fooey";
-        var DeafaultTechnologies = "Foobar";
+        var DefaultTechnologies = "Foobar";
         var DefaultGitHub = "https://github.com/digikidd";
         var DefaultWebAdress = "www.kbccode.com";
 
@@ -15,7 +15,7 @@ class PortfolioItem extends React.Component{
         <div className="portfolioItem col-xs-12 col-sm-4 col-lg-3">
             <h2>{portfolio.name ? portfolio.name : DefaultTitle}</h2>
             <h2>{portfolio.description ? portfolio.description : DefaultDescription}</h2>
-            <h2>{portfolio.technologies ? portfolio.technologies : DeafaultTechnologies}</h2>
+            <h2>{portfolio.technologies ? portfolio.technologies : DefaultTechnologies}</h2>
             <h2>{portfolio.github ? portfolio.github : DefaultGitHub}</h2>
             <h2>{portfolio.webAddress ? portfolio.webAddress : DefaultWebAdress}</h2>
         </div>
